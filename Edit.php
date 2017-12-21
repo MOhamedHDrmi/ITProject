0ade62034a732547ab95fa8082ebfde683fdd4ad
MOhamedHDrmi <hdrmim7@gmail.com>
2017-12-21 23:04:18 +0200

    new V

--- Edit.php
+++ Edit.php
@@ -3,6 +3,8 @@ echo "<style type='text/css'>";
 include'style.css';
 echo "</style>";
 ?>
+<div class="outdiv">
+<div class="editform">
 title :<input name="title" type="text" ><br>
 post :-<br>
 <textarea name="post"  type="text area"></textarea><br>
@@ -10,3 +12,5 @@ select category : <br><select name="category"  type="select">
     <option> cat1
     <option> cat1
 </select>
+</div>
+</div>
